af899d462ba9b0d887bc74d4026a40a2ec0af845
Rohan2002 <rohandeshpande832@gmail.com>
2020-02-08 10:33:33 -0500

    made the location api

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, jsonify, request
 from weather import get_weather_data
 from model import predict
+import json
+import requests
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
+    url = "https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_t8PI3edTMfOr9Q2JkJbok5B300gr8"
+    response = requests.get(url) 
+    x = response.json()
     temp, humidity = get_weather_data('San Jose')
-    return render_template('index.html',x = str(predict(temp,humidity,40.058323,-74.405663)))
+    return render_template('index.html',x = int(predict(temp,humidity,x["location"]["lat"],x["location"]["lng"])))
+
+
+@app.route('/location')
+def index():
+    url = "https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_t8PI3edTMfOr9Q2JkJbok5B300gr8"
+    response = requests.get(url) 
+    x = response.json()
+
+    return str(x["location"]["lat"]) + str(x["location"]["lng"])
 
 if __name__ == "__main__":
     app.run(debug=True,port=10444)
--- weather.py
+++ weather.py
@@ -5,20 +5,6 @@
 # import required modules 
 import requests, json 
 import sys
-# Enter your API key here 
-
-
-# get method of requests module 
-# return response object 
-
-# json method of response object 
-# convert json format data into 
-# python format data 
-
-# Now x contains list of nested dictionaries 
-# Check the value of "cod" key is equal to 
-# "404", means city is found otherwise, 
-# city is not found 
 def get_weather_data(city):
     api_key = "d7049abea5754572b7c3894db82e56e7"
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(city, api_key)
