8753329e425d3e29cb0a116175c7d41e87be7e46
Azfar <azfarmah@outlook.com>
2020-02-08 20:28:07 +0000

    good

--- app.py
+++ app.py
@@ -10,12 +10,11 @@ def home():
     return render_template('home.html')
 @app.route('/demo')
 def demo():
-    url = "https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_t8PI3edTMfOr9Q2JkJbok5B300gr8"
-    response = requests.get(url) 
-    x = response.json()
-    temp, humidity = get_weather_data(x["location"]["lat"],x["location"]["lng"])
+    lat = 43.538797
+    lng = -79.666482
+    temp, humidity = get_weather_data(lat,lng)
     temp = (temp-273.15) * 9/5 + 32
-    return render_template('index.html',x = int(predict(temp,humidity,x["location"]["lat"],x["location"]["lng"])),noise=int(open('noise.txt','r').read()),tem = round(temp,2),hum = round(humidity,2),lat = x["location"]["lat"],long = x["location"]["lng"])
+    return render_template('index.html',x = int(predict(temp,humidity,lat,lng)),noise=int(open('noise.txt','r').read()),tem = round(temp,2),hum = round(humidity,2),lat =lat,long = lng)
 
 
 @app.route('/works')
