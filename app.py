20709ce757871b80810883421ca5222655169ea1
azfar <azfarmah@outlook.com>
2020-02-09 20:55:20 +0000

    new

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def demo():
     lat = 43.538797
     lng = -79.666482
     temp, humidity = get_weather_data(lat,lng)
+    temp = (temp-273.15) * 9/5 + 32
     data = pd.DataFrame({'average_humidity':[17.58,humidity],'type':['average','actual'],'average_temp':[19.76,temp],'noise':[670.6984,int(open('noise.txt','r').read())]})
     graph_humidity = px.bar(data,x="type",y="average_humidity")
     graph_temp = px.bar(data,x='type',y='average_temp')
@@ -24,7 +25,6 @@ def demo():
     graph_humidity = json.dumps(graph_humidity, cls=plotly.utils.PlotlyJSONEncoder)
     graph_temp = json.dumps(graph_temp, cls=plotly.utils.PlotlyJSONEncoder)
     graph_noise = json.dumps(graph_noise, cls=plotly.utils.PlotlyJSONEncoder)
-    temp = (temp-273.15) * 9/5 + 32
     return render_template('index.html',graph_temp=graph_temp,graph_noise=graph_noise,graph_humidity=graph_humidity,x = int(predict(temp,humidity,lat,lng)),noise=int(open('noise.txt','r').read()),tem = round(temp,2), hum = round(humidity,2),lat =lat,long = lng)
 
 
